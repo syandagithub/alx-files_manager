f54e777baca9e2bd556ee1c242f7e557e58d412b
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2024-08-07 18:29:56 +0200

    tired bro

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,53 +1,20 @@
+/* eslint-disable import/no-named-as-default */
 import { v4 as uuidv4 } from 'uuid';
-import sha1 from 'sha1';
 import redisClient from '../utils/redis';
-import userUtils from '../utils/user';
 
-class AuthController {
-  
-  static async getConnect(request, response) {
-    const Authorization = request.header('Authorization') || '';
-
-    const credentials = Authorization.split(' ')[1];
-
-    if (!credentials) { return response.status(401).send({ error: 'Unauthorized' }); }
-
-    const decodedCredentials = Buffer.from(credentials, 'base64').toString(
-      'utf-8',
-    );
-
-    const [email, password] = decodedCredentials.split(':');
-
-    if (!email || !password) { return response.status(401).send({ error: 'Unauthorized' }); }
-
-    const sha1Password = sha1(password);
-
-    const user = await userUtils.getUser({
-      email,
-      password: sha1Password,
-    });
-
-    if (!user) return response.status(401).send({ error: 'Unauthorized' });
-
+export default class AuthController {
+  static async getConnect(req, res) {
+    const { user } = req;
     const token = uuidv4();
-    const key = `auth_${token}`;
-    const hoursForExpiration = 24;
 
-    await redisClient.set(key, user._id.toString(), hoursForExpiration * 3600);
-
-    return response.status(200).send({ token });
+    await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
+    res.status(200).json({ token });
   }
 
-  
-  static async getDisconnect(request, response) {
-    const { userId, key } = await userUtils.getUserIdAndKey(request);
+  static async getDisconnect(req, res) {
+    const token = req.headers['x-token'];
 
-    if (!userId) return response.status(401).send({ error: 'Unauthorized' });
-
-    await redisClient.del(key);
-
-    return response.status(204).send();
+    await redisClient.del(`auth_${token}`);
+    res.status(204).send();
   }
 }
-
-export default AuthController;
